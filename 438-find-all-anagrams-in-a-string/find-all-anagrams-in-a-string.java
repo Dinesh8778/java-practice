a0bd2c61b2fffc77af5df034f7ca6106f8c3a002
Dinesh Kumar S <91470441+Dinesh8778@users.noreply.github.com>
2026-06-18 23:31:55 +0530

    Time: 9 ms (91.82%) | Memory: 46.7 MB (70.69%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -4,30 +4,29 @@ class Solution {
         int n1 = s.length();
         int n2 = p.length();
 
-        // if(n1 <= n2){
-        //     return list;
-        // }
+        if(n2 > n1)
+            return list;
 
-        for(int i = 0; i <= n1 - n2; i++){
-            String sub = s.substring(i, i+n2);
-
-            if(anagram(sub, p)){
-                list.add(i);
-            }
-        }
-
-        return list;
-    }
-
-    private boolean anagram(String s, String p){
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
 
-        for(int i = 0; i < p.length(); i++){
-            freq1[s.charAt(i) - 'a'] ++;
-            freq2[p.charAt(i) - 'a'] ++;
+        for(int i = 0; i < n2; i++){
+            freq1[s.charAt(i) - 'a']++;
+            freq2[p.charAt(i) - 'a']++;
         }
 
-        return Arrays.equals(freq1, freq2);
+        if(Arrays.equals(freq1, freq2))
+            list.add(0);
+
+        for(int i = n2; i < n1; i++){
+
+            freq1[s.charAt(i) - 'a'] ++;
+            freq1[s.charAt(i-n2) - 'a'] --;
+
+            if(Arrays.equals(freq1, freq2))
+                list.add(i - n2 + 1);
+        }
+
+        return list;
     }
 }
