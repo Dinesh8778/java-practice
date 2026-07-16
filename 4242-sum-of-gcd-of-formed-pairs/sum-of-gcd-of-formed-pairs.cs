998a0c668b51e7b1bf06fe5e9214ad962b6183df
Dinesh Kumar S <dineshkumarselvaraj31@gmail.com>
2026-07-16 20:01:47 +0530

    Time: 2085 ms (11.11%) | Memory: 90.7 MB (11.11%) - LeetSync

---
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public long GcdSum(int[] nums) {
+        if(nums.Length == 1)
+            return 0l;
+
+        int[] preGCD = new int[nums.Length];
+        int max = nums[0];
+
+        for(int i = 0 ; i < nums.Length ; i++){
+            max = Math.Max(max,nums[i]);
+            preGCD[i] = GCD(max,nums[i]);
+        }
+
+        foreach(int i in preGCD){
+            Console.Write(i+" ");
+        }
+
+        Array.Sort(preGCD);
+
+        long sum = 0l;
+
+        int l = 0 , r = nums.Length-1;
+
+        while(l<r){
+
+            sum += (long) GCD(preGCD[l], preGCD[r]);
+            l++;
+            r--;
+        }
+
+        return sum;
+    }
+
+    public static int GCD(int a, int b){
+        if(b == 0) return a;
+        return GCD(b,a%b);
+    }
+}
